c4489ccd192e0820c0e440578613e54efa6dd005
松懈 <58425836+oaomumu@users.noreply.github.com>
2024-10-24 22:25:26 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,72 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.ico" }]],
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outlineTitle: "目录",
+    outline: [2, 3],
+    logo:"/logo.ico",
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text:"测试",
+        items:[
+          { text: "首页", link: "/" },
+          { text: "makedown示例", link: '/examples_text'},
+        ],
+      },
+      { text: "front end", link:"/front-end/react/"},
+      { text: "back end", link:"/back-end/rabbitmq/"},      
+    ],
+
+    sidebar: {
+      "/examples_text": set_sidebar("/examples_text"),
+      "/front-end/react": set_sidebar("/front-end/react"),
+      "/back-end/rabbitmq": set_sidebar("/back-end/rabbitmq"),
+    },
+
+    // sidebar: [
+    //   // {
+    //   //   text: 'Examples',
+    //   //   items: [
+    //   //     { text: 'Markdown Examples', link: '/markdown-examples' },
+    //   //     { text: 'Runtime API Examples', link: '/api-examples' }
+    //   //   ]
+    //   // },  
+    // ],
+
+   // 设置搜索框的样式
+   search: {
+    provider: "local",
+    options: {
+      translations: {
+        button: {
+          buttonText: "搜索文档",
+          buttonAriaLabel: "搜索文档",
+        },
+        modal: {
+          noResultsText: "无法找到相关结果",
+          resetButtonTitle: "清除查询条件",
+          footer: {
+            selectText: "选择",
+            navigateText: "切换",
+          },
+        },
+      },
+    },
+  },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/oaomumu/' }
+    ],
+
+    footer:{
+      copyright:"Copyright © 2024 Oaomumu"
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Oaomumu个人知识库"
+  text: "A VitePress Site"
+  tagline: 意外处处发生，新生何时到来？
+  image:
+    src: /background.png
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /examples_text/markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /examples_text/api-examples
+
+features:
+  - title: 🌟 At once
+    details: 在过去成为过去之前，过去也曾是“现在”
+  - title: 🚀 Potency
+    details: 你我均为无限，又何必定义？
+  - title: 🕰️ Fortune 
+    details: 我们拥有世上的全部时间，不是吗？
+---
+
